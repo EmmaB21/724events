a3736a58e0d646b6cd39fb60ad03cba513e4d501
Emma B <emm2111@hotmail.fr>
2023-09-01 11:30:33 +0200

    Modifs formulaire + pagination events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -28,7 +28,7 @@ const EventList = () => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const pageNumber = Math.ceil ((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -60,7 +60,11 @@ const EventList = () => {
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={n}
+                href="#events"
+                onClick={() => setCurrentPage(n + 1)}
+                className={n + 1 === currentPage ? "activePage" : ""}
+              >
                 {n + 1}
               </a>
             ))}
